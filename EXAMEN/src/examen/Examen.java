c2ca96c43813743d7e4130f6742b9120641f4446
Pablo Silva López <pablosilva2705@hotmail.com>
2018-11-29 14:29:11 +0100

    Contador creado

--- EXAMEN/src/examen/Examen.java
+++ EXAMEN/src/examen/Examen.java
@@ -3,8 +3,15 @@ package examen;
 public class Examen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int inicio=1;
+		int max=100;
+		int a=1;
+		int contador=inicio++;
+		do {
+			System.out.println(contador);
+		} while (a!=0);
+		
+		
 	}
 
 }
